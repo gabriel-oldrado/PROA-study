45a220d4f94f16dba2182a8d168655900d9d4289
gabriel.old <gabrieljrg2006@gmail.com>
2026-06-08 07:47:11 -0300

    hdc5 completed

--- atividades/JavaScript/Time2Code/hdc5/hdc5.js
+++ atividades/JavaScript/Time2Code/hdc5/hdc5.js
@@ -2,7 +2,7 @@ export const exercisesHdc5 = [
     {
         id: 1 + " (Externo)",
         external: true,
-        url: "../atividades/JavaScript/Time2Code/hdc5/CaixaEletronico.html",
+        url: "atividades/JavaScript/Time2Code/hdc5/CaixaEletronico.html",
         title: "Caixa Eletrônico",
         desc: "Melhore um sistema de banco existente adicionando segurança e novas funções.",
 
